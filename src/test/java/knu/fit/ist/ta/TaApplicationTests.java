eb6182961bfc3f09e9823e6b33ab5eef4766b09c
User <User@DESKTOP-GBKDGTS>
2021-04-08 22:47:22 +0300

    First commit

---
+++ src/test/java/knu/fit/ist/ta/TaApplicationTests.java
@@ -0,0 +1,13 @@
+package knu.fit.ist.ta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
